fb1b7053d40e5c249affad38bf9862fdbf5dc641
guero <Marouane Guerouji>
2023-03-27 02:07:58 -0400

    netBeans test

--- src/main/java/com/cs321/core/PlayerData.java
+++ src/main/java/com/cs321/core/PlayerData.java
@@ -3,77 +3,85 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.cs321.core;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;
 import java.util.ArrayList;
 import java.io.FileReader;
-import java.util.*; 
+import java.util.*;
 ///////////////////////////////STILL NEEDS TO BE TESTED ///////////////////////////////////////////////
+
 /**
- * class to read and write player's info into and from a file. STILL NEEDS TO BE TESTED 
+ * class to read and write player's info into and from a file. STILL NEEDS TO BE
+ * TESTED
+ *
  * @author James Luna, Hasnain Raza, Marouane Guerouji
  */
-public class PlayerData <A> {
-    /**
-     * ArrayList of gameStat from the file. 
-     */
-    private ArrayList<A> gameStatList= new ArrayList<A>();
-    
-    /**
-     * Player's id. 
-     */
-    private String id=""; 
-    
-    /**
-     * Player's name. 
-     */
-    private String name= ""; 
+public class PlayerData<A> {
 
-    
     /**
-     * method to return player's Id. 
-     * @return  ID. 
+     * ArrayList of gameStat from the file.
      */
-    protected String getPlayerId(){
-    return id; 
-    }
+    private ArrayList<A> gameStatList = new ArrayList<A>();
+
     /**
-     * Method to return player's name. 
-     * @return player's name.  
+     * Player's id.
      */
-    protected String getPlayerName(){
-        return name; 
+    private String id = "";
+
+    /**
+     * Player's name.
+     */
+    private String name = "";
+
+    /**
+     * method to return player's Id.
+     *
+     * @return ID.
+     */
+    protected String getPlayerId() {
+        return id;
     }
-    protected ArrayList<A> gameStat(){
-    return gameStatList; 
+
+    /**
+     * Method to return player's name.
+     *
+     * @return player's name.
+     */
+    protected String getPlayerName() {
+        return name;
     }
-    
+
+    protected ArrayList<A> gameStat() {
+        return gameStatList;
+    }
+
     public void getPlayerData(String filePath) {
-        
+
         try {
             //FileWriter file = new FileWriter(filePath);
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
             String line = reader.readLine();
-            int count =0; 
+            int count = 0;
 
             while (line != null) {
                 //System.out.println(lines);
                 // read next line
-                if (count == 0){
-                    id= line; 
-                  
+                if (count == 0) {
+                    id = line;
+
                 }
-                if (count ==1){
-                name = line; 
+                if (count == 1) {
+                    name = line;
                 }
-                
-                if (count > 1){
-                  gameStatList.add((A) line); 
+
+                if (count > 1) {
+                    gameStatList.add((A) line);
                 }
                 line = reader.readLine();
-                count++; 
+                count++;
             }
 
             reader.close();
@@ -84,23 +92,24 @@ public class PlayerData <A> {
         }
 
     }
-    public void writePlayerData(String filePath, String id, String name, ArrayList<A> gameStat){
-               try {
-      FileWriter file = new FileWriter(filePath);
-      BufferedWriter bufferedWriter = new BufferedWriter(file);
-      bufferedWriter.write(id);
-      bufferedWriter.write(name);
-      for (A s: gameStat){
-          bufferedWriter.write((String) s);
-      
-      }
-      file.close();
-      //System.out.println("Successfully wrote to the file.");
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      
+
+    public void writePlayerData(String filePath, String id, String name, ArrayList<A> gameStat) {
+        try {
+            FileWriter file = new FileWriter(filePath);
+            BufferedWriter bufferedWriter = new BufferedWriter(file);
+            bufferedWriter.write(id);
+            bufferedWriter.write(name);
+            for (A s : gameStat) {
+                bufferedWriter.write((String) s);
+
+            }
+            file.close();
+            //System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+
+        }
+
     }
-    
-    }
-    
+
 }

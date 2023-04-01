4ee8506a16770bbdde755ada54d653dfc47d9be0
guero <Marouane Guerouji>
2023-04-01 07:05:44 -0400

    dart gui

--- src/main/java/com/cs321/gui/dart.java
+++ src/main/java/com/cs321/gui/dart.java
@@ -236,7 +236,7 @@ public class dart extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
+        java.awt.EventQueue.invokeLater(new Runnable() { 
             public void run() {
                 new dart().setVisible(true);
             }

c80340e1453c5de6df4fceaa4cac1b1e0b0a32cc
Hasnain Raza <hasnain.mohsan@gmail.com>
2023-04-11 19:07:22 -0400

    added ability to add players into ffa and teams

--- src/main/java/com/cs321/gui/ChooseGametypePanel.java
+++ src/main/java/com/cs321/gui/ChooseGametypePanel.java
@@ -4,6 +4,16 @@
  */
 package com.cs321.gui;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import com.cs321.core.Player;
 import com.cs321.gui.GUIState.PanelName;
 
 /**
@@ -12,7 +22,15 @@ import com.cs321.gui.GUIState.PanelName;
  */
 public class ChooseGametypePanel extends UpdateableJPanel {
 
+    private static enum Tab {
+        FreeForAll, Teams
+    }
+
     private GUIState state;
+
+    private Tab currentTab = Tab.FreeForAll;
+    private ArrayList<Player> players = new ArrayList<>();
+    private ArrayList<ArrayList<Player>> teams = new ArrayList<>();
 
     /**
      * Creates new form ChooseGametypePanel
@@ -28,7 +46,184 @@ public class ChooseGametypePanel extends UpdateableJPanel {
      */
     @Override
     public void updateComponents() {
+        regenerateFreeForAllPanel();
+        regenerateTeamsPanel();
+    }
 
+    /**
+     * Regenerates the Free For All panel
+     */
+    private void regenerateFreeForAllPanel() {
+        FreeForAllBodyPanel.removeAll();
+
+        // If there are less than 2 players, add 2 buttons
+        if (players.size() < 2) {
+            FreeForAllBodyPanel.add(generateFreeForAllButton(0));
+            FreeForAllBodyPanel.add(new JLabel("vs"));
+            FreeForAllBodyPanel.add(generateFreeForAllButton(1));
+        // Otherwise, add a button for each player and one more
+        } else {
+            for (int playerIndex = 0; playerIndex < players.size(); playerIndex++) {
+                FreeForAllBodyPanel.add(generateFreeForAllButton(playerIndex));
+                FreeForAllBodyPanel.add(new JLabel("vs"));
+            }
+            FreeForAllBodyPanel.add(generateFreeForAllButton(players.size()));
+        }
+
+        FreeForAllBodyPanel.revalidate();
+        FreeForAllBodyPanel.repaint();
+    }
+
+    /**
+     * Generates a button for the Free For All panel
+     */
+    private JButton generateFreeForAllButton(int playerIndex) {
+        JButton button = new JButton("Add Player");
+        if (players.size() > playerIndex) {
+            button.setText(players.get(playerIndex).getName());
+        }
+        button.addActionListener(generateFreeForAllActionListener(playerIndex));
+        return button;
+    }
+
+    /**
+     * Generates an action listener for the Free For All panel
+     */
+    private ActionListener generateFreeForAllActionListener(final int playerIndex) {
+        return new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                String playerName = JOptionPane.showInputDialog("Enter player name");
+                if (playerName.isEmpty()) {
+                    if (players.size() > playerIndex) {
+                        players.remove(playerIndex);
+                    }
+                } else {
+                    if (players.size() > playerIndex) {
+                        players.set(playerIndex, new Player(playerName));
+                    } else {
+                        players.add(new Player(playerName));
+                    }
+                }
+                regenerateFreeForAllPanel();
+            }
+
+        };
+    }
+
+    private void regenerateTeamsPanel() {
+        TeamsBodyPanel.removeAll();
+
+        // If there are less than 2 teams, add 2 team buttons
+        if (teams.size() < 2) {
+            addTeamButtons(0);
+            TeamsBodyPanel.add(generateTeamVsLabel());
+            addTeamButtons(1);
+        // Otherwise, add team buttons for each team and one more
+        } else {
+            for (int teamIndex = 0; teamIndex < teams.size(); teamIndex++) {
+                addTeamButtons(teamIndex);
+                TeamsBodyPanel.add(generateTeamVsLabel());
+            }
+            addTeamButtons(teams.size());
+        }
+
+        TeamsBodyPanel.revalidate();
+        TeamsBodyPanel.repaint();
+    }
+
+    private void addTeamButtons(int teamIndex) {
+        // If the team doesn't exist, add 2 buttons
+        if (teams.size() <= teamIndex) {
+            TeamsBodyPanel.add(generateTeamButton(teams.size(), 0));
+            TeamsBodyPanel.add(new JLabel("and"));
+            TeamsBodyPanel.add(generateTeamButton(teams.size(), 1));
+        // Otherwise, follow the same logic as the Free For All panel
+        } else {
+            ArrayList<Player> team = teams.get(teamIndex);
+
+            // If the team has less than 2 players, add 2 buttons
+            if (team.size() < 2) {
+                TeamsBodyPanel.add(generateTeamButton(teamIndex, 0));
+                TeamsBodyPanel.add(new JLabel("and"));
+                TeamsBodyPanel.add(generateTeamButton(teamIndex, 1));
+            // Otherwise, add a button for each player and one more
+            } else {
+                for (int playerIndex = 0; playerIndex < team.size(); playerIndex++) {
+                    TeamsBodyPanel.add(generateTeamButton(teamIndex, playerIndex));
+                    TeamsBodyPanel.add(new JLabel("and"));
+                }
+                TeamsBodyPanel.add(generateTeamButton(teamIndex, team.size()));
+            }
+        }
+    }
+
+    private JButton generateTeamButton(int teamIndex, int playerIndex) {
+        JButton button = new JButton("Add Player");
+        button.addActionListener(generateTeamActionListener(teamIndex, playerIndex));
+
+        // If the team exists ...
+        if (teams.size() > teamIndex) {
+            ArrayList<Player> team = teams.get(teamIndex);
+
+            // ... and the player exists, set the button text to the player's name
+            if (team.size() > playerIndex) {
+                button.setText(team.get(playerIndex).getName());
+            }
+        }
+
+        return button;
+    }
+
+    private ActionListener generateTeamActionListener(final int teamIndex, final int playerIndex) {
+        return new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                String playerName = JOptionPane.showInputDialog("Enter player name");
+                // If the player name is empty ...
+                if (playerName.isEmpty()) {
+                    // ... and the team exists ...
+                    if (teams.size() > teamIndex) {
+                        ArrayList<Player> team = teams.get(teamIndex);
+                        // ... and the player exists, remove the player
+                        if (team.size() > playerIndex) {
+                            team.remove(playerIndex);
+                        }
+                        // If the team becomes empty, remove the team
+                        if (team.isEmpty()) {
+                            teams.remove(teamIndex);
+                        }
+                    }
+                } else {
+                    // If the team exists ...
+                    if (teams.size() > teamIndex) {
+                        ArrayList<Player> team = teams.get(teamIndex);
+                        // ... and the player exists, set the player's name
+                        if (team.size() > playerIndex) {
+                            team.set(playerIndex, new Player(playerName));
+                        // ... and the player doesn't exist, add the player
+                        } else {
+                            team.add(new Player(playerName));
+                        }
+                    // If the team doesn't exist, add the team
+                    } else {
+                        ArrayList<Player> team = new ArrayList<>();
+                        team.add(new Player(playerName));
+                        teams.add(team);
+                    }
+                }
+                regenerateTeamsPanel();
+            }
+
+        };
+    }
+
+    private JLabel generateTeamVsLabel() {
+        JLabel label = new JLabel("vs");
+        label.setFont(getFont().deriveFont(Font.BOLD, 24));
+        return label;
     }
 
     /**
@@ -105,6 +300,11 @@ public class ChooseGametypePanel extends UpdateableJPanel {
 
         GametypesTabbedPanel.setMinimumSize(new java.awt.Dimension(1, 1));
         GametypesTabbedPanel.setPreferredSize(new java.awt.Dimension(1, 1));
+        GametypesTabbedPanel.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                GametypesTabbedPanelStateChanged(evt);
+            }
+        });
 
         FreeForAllTabPanel.setMinimumSize(new java.awt.Dimension(1, 1));
         FreeForAllTabPanel.setPreferredSize(new java.awt.Dimension(1, 1));
@@ -131,6 +331,11 @@ public class ChooseGametypePanel extends UpdateableJPanel {
         FreeForAllFooterPanel.add(filler22);
 
         FreeForAllClearButton.setText("Clear");
+        FreeForAllClearButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                FreeForAllClearButtonActionPerformed(evt);
+            }
+        });
         FreeForAllFooterPanel.add(FreeForAllClearButton);
 
         FreeForAllPanel.add(FreeForAllFooterPanel);
@@ -169,6 +374,11 @@ public class ChooseGametypePanel extends UpdateableJPanel {
         TeamsFooterPanel.add(filler25);
 
         TeamsClearButton.setText("Clear");
+        TeamsClearButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TeamsClearButtonActionPerformed(evt);
+            }
+        });
         TeamsFooterPanel.add(TeamsClearButton);
 
         TeamsPanel.add(TeamsFooterPanel);
@@ -228,15 +438,44 @@ public class ChooseGametypePanel extends UpdateableJPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void QuitToMainMenuButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_QuitToMainMenuButtonActionPerformed
-        // TODO add your handling code here:
         state.panels.get(PanelName.MainMenuPanel).updateComponents();
         state.contentPaneCardLayout.show(state.contentPane, PanelName.MainMenuPanel.toString());
         
     }//GEN-LAST:event_QuitToMainMenuButtonActionPerformed
 
     private void NextButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NextButtonActionPerformed
-        // TODO add your handling code here:
+        state.panels.get(PanelName.Dart_2).updateComponents();
+        state.contentPaneCardLayout.show(state.contentPane, PanelName.Dart_2.toString());
     }//GEN-LAST:event_NextButtonActionPerformed
+
+    private void FreeForAllClearButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_FreeForAllClearButtonActionPerformed
+        players.clear();
+        updateComponents();
+    }//GEN-LAST:event_FreeForAllClearButtonActionPerformed
+
+    private void GametypesTabbedPanelStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_GametypesTabbedPanelStateChanged
+        int index = GametypesTabbedPanel.getSelectedIndex();
+        switch (index) {
+            case 0:
+                if (currentTab != Tab.FreeForAll) {
+                    players.clear();
+                }
+                currentTab = Tab.FreeForAll;
+                break;
+            case 1:
+                if (currentTab != Tab.Teams) {
+                    teams.clear();
+                }
+                currentTab = Tab.Teams;
+                break;
+        }
+        updateComponents();
+    }//GEN-LAST:event_GametypesTabbedPanelStateChanged
+
+    private void TeamsClearButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TeamsClearButtonActionPerformed
+        teams.clear();
+        updateComponents();
+    }//GEN-LAST:event_TeamsClearButtonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables

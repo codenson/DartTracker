7cb591d81885960eb5d066e2762dc3da07e3f813
James Luna <jluna4@gmu.edu>
2023-04-12 14:45:46 -0400

    Implemented IOUtils for player and gamestats.
    Co-authored-by: Hasnain Raza <Hasnain123Raza@users.noreply.github.com>

--- src/main/java/com/cs321/io/PlayerDTO.java
+++ src/main/java/com/cs321/io/PlayerDTO.java
@@ -1,5 +1,18 @@
 package com.cs321.io;
 
+import java.util.ArrayList;
+
+/**
+ * A data transfer object for the Player class.
+ * 
+ * @author James Luna, Hasnain Raza, Marouane Guerouji
+ */
 public class PlayerDTO {
-    
+
+    /** The ID of this player. */
+    public String id;
+    /** The name of this player. */
+    public String name;
+    /** The gamestats of the player from previous games. */
+    public GameStatsDTO[] gameStats;
 }
